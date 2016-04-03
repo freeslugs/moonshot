068451eeef8564ad281edfbf6a31bd65cdc6d90e
Parthiban Loganathan <parthibanloganathan@users.noreply.github.com>
2016-04-03 03:20:08 -0400

    Start adding serial

--- demo.js
+++ demo.js
@@ -1,11 +1,27 @@
-  var socket = require('socket.io-client')('http://moonshot.ngrok.com');
-  
-  socket.on('connect', function(){
-    socket.emit('set demo');
+var socket = require('socket.io-client')('http://moonshot.ngrok.com');
+
+socket.on('connect', function(){
+  socket.emit('set demo');
+});
+
+socket.on('rotation', function (data) {
+  var SerialPort = require("serialport").SerialPort;
+  var portName = '/dev/tty.usbmodem1411';
+
+  var port = new SerialPort(portName, {
+    baudRate: 9600,
+    parser: serialport.parsers.readline("\n")
   });
-  socket.on('rotation', function(data){
+
+  serialPort.on('open', function () {
+    serialPort.write('ls\n', function(err, results) {
+      console.log('err ' + err);
+      console.log('results ' + results);
+    });
+  });
+
+  serialPort.on('data', function () {
+    serialPort.write(data);
     console.log(data);
   });
-  socket.on('disconnect', function(){
-
-  });
+});
