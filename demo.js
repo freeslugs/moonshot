4994821b3e18e1b27e6140ee30d230e20a1ec8d8
freeslugs <freeslugs@gmail.com>
2016-04-03 01:46:48 -0400

    demo server

---
+++ demo.js
@@ -0,0 +1,10 @@
+  var socket = require('socket.io-client')('http://moonshot.ngrok.com');
+  socket.on('connect', function(){
+    
+  });
+  socket.on('event', function(data){
+
+  });
+  socket.on('disconnect', function(){
+
+  });
